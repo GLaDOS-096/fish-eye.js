0d36d07e7ad0cdc85febfbbb9406f2e9ded05547
GLaDOS-096 <ZhangYf_CPU@163.com>
2017-04-07 23:35:45 +0800

    second commit

--- fish-eye.js
+++ fish-eye.js
@@ -1,34 +1,47 @@
 /* Object design:
-   Eye{
-       level: the higher it is, the higher level it will be
+   Node{
+       name: object of this very module
+       exps: array of modules that depend on this module
+       deps: array of dependencies of this module  
        radius: the radius of this circle
        x: posX of the circle
        y: posY of the circle
        color: color string
    }
-   DepNode{
-       name: object of this very module
-       exps: array of modules that depend on this module
-       deps: array of dependencies of this module                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+   Graph{
+       scope: array of layers
+       init(,method,dir): function to read the dependency tree
+       setScope(): the function to set the                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
    }
    Camera{
-       center: it be the center of the camera as said
-       scope: array of layers
-       formColor(): function that forms a color
+       target: the graph it is looking at
+       setScope(scope): the function to set the scope to 
    }
  */
 
-function Eye(eye){
+function Node(node){
     var self = this
-    this.x = eye.x
-    this.y = eye.y
-    this.color = eye.color
+    this.name = node.name
+    this.exps = node.exps
+    this.deps = node.deps
+    this.radius = node.radius
+    this.x = node.x
+    this.y = node.y
+    this.color = node.color ||
+        (function(){
+            var __r__ = Math.floor(Math.random()*255)
+            var __g__ = Math.floor(Math.random()*255)
+            var __b__ = Math.floor(Math.random()*255)
+            return '#'+__r__.toString(16)+__g__.toString(16)+__b__.toString(16)
+        })()
 }
 
-function DepNode{
-
+function Graph(){
+    this.scope = []
+    this.init = function init(method,dir){}
 }
 
-function Camera{
-
+function Camera(target){
+    this.target = target
+    this.setScope = function setScope(){}
 }
